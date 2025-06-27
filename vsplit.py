c072c0169a32093c8780e2fcae1d940456fb96cd
mushfiq <mushfiqxrabbi@gmail.com>
2025-06-28 04:30:55 +0600

    feat: add option to open splits in home directory
    - Added `--cwd-home` option to `hsplit.py` and `vsplit.py`.
    - Added keybindings to `kitty.conf` to use this option.

--- vsplit.py
+++ vsplit.py
@@ -1,3 +1,4 @@
+import os
 from kitty.boss import Boss
 
 
@@ -18,11 +19,12 @@ from kittens.tui.handler import result_handler
 def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
     tab = boss.active_tab
     w = boss.window_id_map.get(target_window_id)
+    cwd = "--cwd-home" in args
     if tab.title == "nvim":
         boss.call_remote_control(w, ('goto-layout', 'splits'))
-        boss.call_remote_control(w, ('launch', '--type=window', '--location=vsplit', '--bias=25', '--cwd=current'))
+        boss.call_remote_control(w, ('launch', '--type=window', '--location=vsplit', '--bias=25', f"--cwd={os.path.expanduser("~") if cwd else "current"}"))
     else:
         boss.call_remote_control(w, ('goto-layout', 'splits'))
-        boss.call_remote_control(w, ('launch', '--type=window', '--location=vsplit', '--bias=50', '--cwd=current'))
+        boss.call_remote_control(w, ('launch', '--type=window', '--location=vsplit', '--bias=50', f"--cwd={os.path.expanduser("~") if cwd else "current"}"))
 
     # print(json.dumps(w, indent=2, cls=CustomEncoder))
